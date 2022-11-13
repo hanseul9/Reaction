5fe08638a69554bbdb22513f098f70fe7caa4b5b
jeongmirr <115198864+jeongmirr@users.noreply.github.com>
2022-11-14 02:48:29 +0900

    sidebar, marker add realization

--- map/src/components/map/Map.js
+++ map/src/components/map/Map.js
@@ -2,7 +2,6 @@
 import React, { useEffect, useState } from "react";
 import Sidebar from "../sidebar/Sidebar";
 import MarkerModal from "./MarkerModal";
-//import positions from "./positions.json";
 
 const Map = () => {
   const [sidebarData, setSidebarData] = useState({});

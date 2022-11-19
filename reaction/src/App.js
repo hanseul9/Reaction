7b8f658579478bbf09faefbb34c89d6e8bc9c077
brekJ <118181118+hs-1891304-jaehoonjeong@users.noreply.github.com>
2022-11-19 18:47:21 +0900

    no message

--- reaction/src/App.js
+++ reaction/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import MainPageComponents from './components/MainPageComponents.js';
+import {  Routes, Route, Navigate  } from "react-router-dom";
+import Login from "./components/Login/Login";
+import Signup from "./components/Signup/Signup";
+import ErrorPage from "./components/error/ErrorPage";
+import Map from "./components/map/Map";
+import StarRating from "./components/sidebar/StarRating"
+import Sidebar from "./components/sidebar/Sidebar"
+import MyRoomComponents from './components/MyRoomComponents.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+let username="profile"
+let email="email"
+let etc="etc"
+
+function App(){
+  
+return(
+
+  
+  
+      
+      <Routes>
+        <Route path="/" element={<MainPageComponents/>}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/login/signup/*" element={<Signup />} />
+        <Route path="/*" element={<ErrorPage />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="/sidebar" element={<Sidebar />} />
+        <Route path="/myroom" element={<MyRoomComponents username = {username} email = {email} etc={etc}/>} />
+      </Routes>
   );
 }
-
 export default App;
---
+++ reaction/src/components/sidebar/StarRating.js
@@ -0,0 +1,13 @@
+/* color-org/src/components/StarRating.js */
+import React from "react";
+import { FaStar } from "react-icons/fa";
+
+export default function StarRating(){
+    return [
+    <FaStar color="red" />,
+    <FaStar color="red" />,
+    <FaStar color="red" />,
+    <FaStar color="grey" />,
+    <FaStar color="grey" />
+    ];
+}

98acb86261d311ea1838dfc2e2dbf3c559c05202
jeongmirr <115198864+jeongmirr@users.noreply.github.com>
2022-11-19 17:09:40 +0900

    marker address add realization

--- map/src/components/map/Map.js
+++ map/src/components/map/Map.js
@@ -2,46 +2,55 @@
 import React, { useEffect, useState } from "react";
 import Sidebar from "../sidebar/Sidebar";
 import MarkerModal from "./MarkerModal";
+import positions from "./positions.json";
 
 const Map = () => {
   const [sidebarData, setSidebarData] = useState({});
   const [isClickMarker, setIsClickMarker] = useState(false);
   const [latitude, setLatitude] = useState();
   const [longitude, setLongitude] = useState();
-  const [ is_modal, setModal ] = useState(false);
-  
+  const [is_modal, setModal] = useState(false);
+  const [positionData, setPositionData] = useState(positions);
+  const [address, setAddress] = useState("");
+  const [isCloseModal, setIsCloseModal] = useState(false);
+
   useEffect(() => {
     var container = document.getElementById("map"); // 지도 표시할 div
     var options = {
       center: new kakao.maps.LatLng(37.58575786444733, 127.0098896998893), // 지도 중심 좌표
       level: 4, // 지도 확대 레벨
+      disableDoubleClickZoom: true, // 지도 더블 클릭시 확대 금지
     };
 
     // 지도 생성
     var map = new kakao.maps.Map(container, options);
-
     // 마커 이미지 주소
     var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
 
-    for (var i = 0; i < positions.length; i++) {
-      // 마커 이미지 크기
-      var imageSize = new kakao.maps.Size(24, 35);
+    // 마커 이미지 크기
+    var imageSize = new kakao.maps.Size(24, 35);
 
-      // 마커 이미지 생성
-      var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+    // 마커 이미지 생성
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
 
+    var newMarker = new kakao.maps.Marker({
+      map: map, // 마커를 표시할 지도
+      image: markerImage,
+    });
+
+    for (var i = 0; i < positionData.length; i++) {
       // 마커 생성
       var marker = new kakao.maps.Marker({
         map: map, // 마커를 표시할 지도
-        position: new kakao.maps.LatLng(positions[i].lat, positions[i].lng), // 마커의 위치
+        position: new kakao.maps.LatLng(positionData[i].lat, positionData[i].lng), // 마커의 위치
         image: markerImage, // 마커 이미지
       });
 
       // 마커에 표시 인포윈도우 생성
       var infowindow = new kakao.maps.InfoWindow({
         content:
-          `<div style="padding:5px; width:100%"><div style="font-weight: 600; margin-bottom: 3px;">${positions[i].content}` +
-          `<div style="font-weight: 500; margin-bottom: 3px;">${positions[i].locate}`, // 인포윈도우 표시할 내용
+          `<div style="padding:5px; width:100%"><div style="font-weight: 600; margin-bottom: 3px;">${positionData[i].content}` +
+          `<div style="font-weight: 500; margin-bottom: 3px;">${positionData[i].locate}`, // 인포윈도우 표시할 내용
       });
 
       // 마커에 mouseover 이벤트, mouseout 이벤트 등록
@@ -50,7 +59,10 @@ const Map = () => {
       kakao.maps.event.addListener(marker, "mouseover", makeOverListener(map, marker, infowindow));
       kakao.maps.event.addListener(marker, "mouseout", makeOutListener(infowindow));
 
-      const sidebarInfo = { storeTitle: positions[i].title, placeId: positions[i].placeId };
+      const sidebarInfo = {
+        storeTitle: positionData[i].title,
+        placeId: positionData[i].placeId,
+      };
       // 마커에 클릭 이벤트 등록
       kakao.maps.event.addListener(marker, "click", function () {
         setSidebarData(sidebarInfo);
@@ -72,33 +84,61 @@ const Map = () => {
       };
     }
 
-    // 지도 클릭 시 마커 표시하기
-    kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
+    // 추가한 마커의 인포윈도우
+    var addedInfoWindow = new kakao.maps.InfoWindow({
+      content:
+        `<div  style="padding:5px; width:100%; cursor:pointer"><div style="font-weight: 600; margin-bottom: 3px;"><div id='addedInfoWindow'>새로운 장소 추가하기</div>` +
+        `<div style="font-weight: 500; margin-bottom: 3px;">`,
+    });
+
+    // 지도 더블 클릭 시 마커 표시하기
+    kakao.maps.event.addListener(map, "dblclick", function (mouseEvent) {
       // 클릭한 위도, 경도 정보를 가져옵니다.
       const latlng = mouseEvent.latLng;
-      
+
       // 위도 경도 값을 useState를 이용해서 useEffect 밖으로 빼냅니다.
-      setLatitude(latlng.getLat())
-      setLongitude(latlng.getLng())
-      
-      // 마커 위치를 클릭한 위치로 옮깁니다.
-      marker.setPosition(latlng);
-      
-      // 마커를 지도상에 보여줍니다.
-      marker.setMap(map);
-    })
+      setLatitude(latlng.getLat());
+      setLongitude(latlng.getLng());
+
+      newMarker.setPosition(latlng);
+      kakao.maps.event.addListener(
+        newMarker,
+        "mouseover",
+        makeOverListener(map, newMarker, addedInfoWindow)
+      );
+
+      // 좌표로 주소를 알아내는 함수
+      function getAddr(lat, lng) {
+        const geocoder = new kakao.maps.services.Geocoder();
+
+        let coord = new kakao.maps.LatLng(lat, lng);
+        let callback = function (result, status) {
+          if (status === kakao.maps.services.Status.OK) {
+            setAddress(result[0].road_address.address_name);
+          }
+        };
+        geocoder.coord2Address(coord.getLng(), coord.getLat(), callback);
+      }
+      getAddr(latlng.getLat(), latlng.getLng());
+    });
 
     // 마커에 클릭이벤트를 등록하기
-    kakao.maps.event.addListener(marker, 'click', function(){
-      // 마커 생성 모달창을 띄워준다. 
-      setModal(true)
-    })
+    kakao.maps.event.addListener(newMarker, "click", function () {
+      // 마커 생성 모달창을 띄워준다.
+      setModal(true);
+    });
 
-  });
+    // 장소를 추가하지않고 모달창을 닫으면 생성됐던 마커도 사라짐
+    if (isCloseModal) {
+      newMarker.setMap(null);
+      setIsCloseModal(false);
+    }
+  }, [positionData, isCloseModal]);
 
   const closeModal = () => {
-    setModal(false)
-  }
+    setModal(false);
+    setIsCloseModal(true);
+  };
 
   return (
     <div>
@@ -108,8 +148,15 @@ const Map = () => {
         isClickMarker={isClickMarker}
         clickMarkerHandler={setIsClickMarker}
       />
-      {is_modal? <MarkerModal close={closeModal} latitude={latitude} longitude={longitude} />
-      : null }
+      {is_modal ? (
+        <MarkerModal
+          close={closeModal}
+          setPositionData={setPositionData}
+          latitude={latitude}
+          longitude={longitude}
+          address={address}
+        />
+      ) : null}
     </div>
   );
 };
--- map/src/components/map/MarkerModal.js
+++ map/src/components/map/MarkerModal.js
@@ -1,118 +1,121 @@
-import React, {useCallback, useState} from 'react';
-import TextField from '@material-ui/core/TextField';
-import CloseIcon from '@material-ui/icons/Close';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import CloseIcon from "@material-ui/icons/Close";
+import styled from "styled-components";
 
-const MarkerModal =(props) => {
+const MarkerModal = ({ setPositionData, longitude, latitude, close, address }) => {
+  // 사용자가 지정한 마커 이름을 담아냅니다.
+  const [title, setTitle] = useState();
 
-    // 사용자가 지정한 마커 이름을 담아냅니다.
-    const [ title, setTitle ] = useState()
+  const selectTitle = (e) => {
+    console.log(e.target.value);
+    setTitle(e.target.value);
+  };
 
-
-    const selectTitle = (e) => {
-        console.log(e.target.value)
-        setTitle(e.target.value)
-    }
-
-    // 마커를 생성할 때 사용되는 함수입니다.
-    const addMarker = () => {
-        let marker = {
-        // Map.js에서 받아온 값도 있고 여기서 생성한 값도 있습니다.
-        latitude: props.latitude,
-        longitude : props.longitude,
-        title: title,
-    }
+  // 마커를 생성할 때 사용되는 함수입니다.
+  // positions라는 상태변수에 그 위치의 정보를 저장하면 useEffect가 다시 한번 실행되면서 마커가 렌더됨
+  const addMarker = () => {
+    const clickedPosition = {
+      title: title,
+      content: `<div>${title}</div>`,
+      locate: `<div>${address}</div>`,
+      lat: latitude,
+      lng: longitude,
+    };
+    setPositionData((prev) => [...prev, { placeId: prev.length, ...clickedPosition }]);
 
     // 마커 생성 모달 창을 닫습니다.
-    props.close()
-    }
+    close();
+  };
 
-    return(
-        <React.Fragment>
-            <Component onClick={props.close} />
-            <ModalComponent>
-                <ModalExitBtn onClick={props.close} >
-                    <CloseIcon/>
-            </ModalExitBtn>
-            <ModalHeader>마커를 생성하시겠습니까?</ModalHeader>
-            <ModalInput>
-                <TextField id="standard-basic" label="장소를 입력해주세요." style={{width: '100%'}} onChange={selectTitle} />
-                
-            </ModalInput>
-            <ModalButtonContainer onClick={() => alert('권한이 없습니다.')}>
-                <ModalSubmitBtn onClick={addMarker}>
-                    마커 생성
-                </ModalSubmitBtn >
-            </ModalButtonContainer>
-            </ModalComponent>
-        </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      <Component onClick={close} />
+      <ModalComponent>
+        <ModalExitBtn onClick={close}>
+          <CloseIcon />
+        </ModalExitBtn>
+        <ModalHeader>마커를 생성하시겠습니까?</ModalHeader>
+        <ModalInput>
+          <TextField
+            id="standard-basic"
+            label="장소를 입력해주세요."
+            style={{ width: "100%" }}
+            onChange={selectTitle}
+          />
+        </ModalInput>
+        <ModalButtonContainer>
+          <ModalSubmitBtn onClick={addMarker}>마커 생성</ModalSubmitBtn>
+        </ModalButtonContainer>
+      </ModalComponent>
+    </React.Fragment>
+  );
+};
 
 const Component = styled.div`
-    position: fixed;
-    top: 0;
-    opacity: 0.4;
-    height: 100vh;
-    width: 100vw;
-    background-color: black;
-    z-index: 10;
-`
+  position: fixed;
+  top: 0;
+  opacity: 0.4;
+  height: 100vh;
+  width: 100vw;
+  background-color: black;
+  z-index: 10;
+`;
 
 const ModalComponent = styled.div`
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    width: 500px;
-    height: 300px;
-    transform: translate(-50%, -50%);
-    background-color: white;
-    z-index: 20;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  width: 500px;
+  height: 300px;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  z-index: 20;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 const ModalHeader = styled.div`
-    margin-top: 30px;
-    font-weight: 600;
-    font-size: 18px;
-`
+  margin-top: 30px;
+  font-weight: 600;
+  font-size: 18px;
+`;
 
 const ModalInput = styled.div`
-    box-sizing: border-box; 
-    width: 50%;
-`
+  box-sizing: border-box;
+  width: 50%;
+`;
 const ModalButtonContainer = styled.div`
-    box-sizing: border-box; 
-    width: 50%;
-    margin-bottom: 30px;
-`
+  box-sizing: border-box;
+  width: 50%;
+  margin-bottom: 30px;
+`;
 const ModalSubmitBtn = styled.button`
-    width: 100%;
-    background-color: #4DCEF5;
-    border: none;
-    outline: none;
-    padding: 10px 0;
-    cursor: pointer;
-    font-weight: 600;
-    font-size: 15px;
-    border-radius: 4px;
-    &:hover {
-        opacity: 0.7;
-    }
-`
+  width: 100%;
+  background-color: #4dcef5;
+  border: none;
+  outline: none;
+  padding: 10px 0;
+  cursor: pointer;
+  font-weight: 600;
+  font-size: 15px;
+  border-radius: 4px;
+  &:hover {
+    opacity: 0.7;
+  }
+`;
 const ModalExitBtn = styled.button`
-    position: absolute;
-    top: 0;
-    right: 0;
-    padding: 8px 12px;
-    cursor: pointer;
-    background-color: transparent;
-    outline: none;
-    border: none;
-    color: black;
-`
+  position: absolute;
+  top: 0;
+  right: 0;
+  padding: 8px 12px;
+  cursor: pointer;
+  background-color: transparent;
+  outline: none;
+  border: none;
+  color: black;
+`;
 
-export default MarkerModal
+export default MarkerModal;

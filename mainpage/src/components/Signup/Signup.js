f9b5bcca149ec98be3e6cd77a4a61849b1febeea
hs-1871417-suhanlim <suhan128@naver.com>
2022-11-19 18:32:37 +0900

    jsonData update

--- mainpage/src/components/Signup/Signup.js
+++ mainpage/src/components/Signup/Signup.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import { useState } from 'react';
 import { useEffect } from 'react';
-import { useLocation, Link } from "react-router-dom"; 
+import { Link } from "react-router-dom"; 
+import styles from './SignupStyle.module.css';
+import UserData from "../jsonData/userData.json"; //데이터 정보
 
-//회원정보 더미 데이터
-const User = {
-    email : 'reaction@hansung.ac.kr',
-    pw : 'qwerty123@',
-    birth : 19991111,
-    name : 'Limsuhan'
-}
+
 
 //회원가입 컴포넌트
 export default function Signup() {
@@ -27,6 +23,8 @@ export default function Signup() {
     const [nameValid, setNameValid] = useState(false); //이름 형식이 맞는지
     const [notAllow, setNotAllow] = useState(true);//버튼 활성화
    
+    var overlapEmail = new Array(); //배열 선언
+
     const handleEmail = (e) => {
       setEmail(e.target.value);
       const regex =//                     자바스크립트 정규표현식
@@ -86,50 +84,58 @@ export default function Signup() {
         setNotAllow(true); //기본적으로 버튼 비활성화
       }, [emailValid, pwValid, pwcheckValid, birthValid, nameValid]);
 
-    const onClickConfirmButton = () => { //회원가입 버튼을 눌렀을때 액션
-      if(email === User.email && pw === User.pw && name===User.name) {
-        alert('로그인에 성공했습니다.')
-      } else {
-        alert("등록되지 않은 회원입니다.");
+    const SignupConfirmButton = () => { //회원가입 버튼을 눌렀을때 액션
+      if(!notAllow) {//회원가입 버튼이 활성화 됐다면 (각 입력칸의 조건이 맞다면.)
+        alert('회원가입에에 성공했습니다.');
+        document.location = '/map';//회원가입이 성공되면 맵으로 이동
+      } else {//예외처리
+        alert("오류가 발생했습니다.");
+        document.location = '/*'; //오류 페이지로 이동
       }
     }
 
     const overlapCheckButton = () => { //중복확인 버튼을 눌렀을때 액션
-      if(email === User.email) {
+      overlapEmail = UserData.map((UserData, idx) => {
+        overlapEmail = UserData.userEmail;//overlapEmail에 모든 userEmail이 들어감
+        //console.log(overlapEmail);
+        return overlapEmail;
+      }
+      );
+      if(overlapEmail.indexOf(email) != -1) {
+        //console.log(email);
+        //console.log(overlapEmail);
         alert('이미 사용중인 아이디 입니다.');
+        setEmailValid(false); //사용중이니까 오류문구 출력
       } else {
         alert("사용 가능한 아이디 입니다.");
+        setEmailValid(true);
       }
     } 
 
-    const GoBackButton = () => {
-      
-    }
-
     return (
-        <div className="Spage">
+        <div className={styles.Spage}>
           <Link to="/login"> {/*로그인 페이지로 뒤로가기*/}
-            <button className='backbutton' onClick={GoBackButton}>
+            <button className={styles.backbutton}>
               ❮
             </button>
           </Link>
 
-          <div className="titleWrap1">
+          <div className={styles.titleWrap1}>
             회원가입
           </div>
     
-          <div className="contentWrap">
-            <div className="inputTitle">이메일</div>
-            <div className="inputWrap">
+          <div className={styles.contentWrap}>
+            <div className={styles.inputTitle}>이메일</div>
+            <div className={styles.inputWrap}>
               <input 
               type= 'text'
-              className="input" 
+              className={styles.input}
               placeholder='reaction@hansung.ac.kr'
               value={email}
               onChange={handleEmail}/>
-              <button onClick={overlapCheckButton} className='overlapCheck'>중복확인</button>
+              <button onClick={overlapCheckButton} className={styles.overlapCheck}>중복확인</button>
             </div>
-            <div className="errorMessageWrap">
+            <div className={styles.errorMessageWrap}>
               {//이메일 길이가 0을 넘고 이메일 형식이 맞지 않으면 출력
                 !emailValid && email.length > 0 && (
                     <div>올바른 이메일을 입력해주세요.</div>
@@ -138,16 +144,16 @@ export default function Signup() {
               }
             </div>
     
-            <div style={{marginTop:"26px"}} className="inputTitle">비밀번호</div >
-            <div className="inputWrap">
+            <div style={{marginTop:"26px"}} className={styles.inputTitle}>비밀번호</div >
+            <div className={styles.inputWrap}>
               <input 
               type= 'password'
-              className="input" 
+              className={styles.input}
               placeholder='영문, 숫자, 특수문자 8자 이상'
               value={pw}
               onChange={handlePw}/>
             </div>
-            <div className="errorMessageWrap">
+            <div className={styles.errorMessageWrap}>
               {//비밀번호 길이가 0을 넘고 비밀번호 형식이 맞지 않으면 출력
                 !pwValid && pw.length > 0 && (
                     <div>영문, 숫자, 특수문자 8자 이상 입력해주세요.</div>
@@ -155,47 +161,47 @@ export default function Signup() {
               }
             </div>
             <div style={{marginTop:"26px"}} className="inputTitle">비밀번호 확인</div >
-            <div className="inputWrap">
+            <div className={styles.inputWrap}>
               <input 
               type= 'password'
-              className="input" 
+              className={styles.input}
               placeholder='영문, 숫자, 특수문자 8자 이상'
               value={pwcheck}
               onChange={handlePwcheck}/>
             </div>
-            <div className="errorMessageWrap">
+            <div className={styles.errorMessageWrap}>
               {//비밀번호 확인 길이가 0을 넘고 비밀번호가 맞지 않을때 출력
                 !pwcheckValid && pwcheck.length > 0 && (
                     <div>비밀번호가 정확하지 않습니다.</div>
                 )
               }
             </div>
-            <div style={{marginTop:"26px"}} className="inputTitle">이름</div >
-            <div className="inputWrap">
+            <div style={{marginTop:"26px"}} className={styles.inputTitle}>이름</div >
+            <div className={styles.inputWrap}>
               <input 
               type= 'text'
-              className="input" 
+              className={styles.input}
               placeholder='이름 입력'
               value={name}
               onChange={handleName}/>
             </div>
-            <div className="errorMessageWrap">
+            <div className={styles.errorMessageWrap}>
               {//이름을 썼는데, 이름 길이가 2자 미만이거나 5자 이상일 경우 출력
                 !nameValid && name.length > 0 && (
                     <div>2글자 이상 5글자 미만으로 입력해주세요.</div>
                 )
               }
             </div>
-            <div style={{marginTop:"26px"}} className="inputTitle">생년월일</div >
-            <div className="inputWrap">
+            <div style={{marginTop:"26px"}} className={styles.inputTitle}>생년월일</div >
+            <div className={styles.inputWrap}>
               <input 
               type= 'text'
-              className="input" 
+              className={styles.input} 
               placeholder='YYYYMMDD'
               value={birth}
               onChange={handleBirthday}/>
             </div>
-            <div className="errorMessageWrap">
+            <div className={styles.errorMessageWrap}>
               {//생년월일 길이가 0을 넘고 형식에 맞지 않으면 출력
                 !birthValid && birth.length > 0 && (
                     <div>YYYYMMDD 형식으로 입력 해주세요.</div>
@@ -206,7 +212,7 @@ export default function Signup() {
     
           <div>
           <Link to="/map">{/*맵 화면으로 가기*/}
-            <button onClick={onClickConfirmButton} disabled={notAllow} className='bottomButton'>
+            <button onClick={SignupConfirmButton} disabled={notAllow} className={styles.SbottomButton}>
               회원가입 완료
             </button>
           </Link>
